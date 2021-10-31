3965109ab90ea13c451ca940c0bd6858a84c9597
Leo Gunadarma <67279463+Leo-gunadarma@users.noreply.github.com>
2021-10-31 14:40:41 +0800

    Menu kelola hewan

---
+++ app/src/main/java/id/leo/ternaku/KelolaHewanActivity.java
@@ -0,0 +1,29 @@
+package id.leo.ternaku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class KelolaHewanActivity extends AppCompatActivity {
+    FloatingActionButton addButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelola_hewan);
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tambahData();
+            }
+        });
+    }
+    public void tambahData(){
+        Intent intent= new Intent(KelolaHewanActivity.this , TambahHewanActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/id/leo/ternaku/MainActivity.java
+++ app/src/main/java/id/leo/ternaku/MainActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView kelolaHewan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        kelolaHewan = findViewById(R.id.imageViewKelolaHewan);
+        kelolaHewan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuKelolaHewan();
+            }
+        });
+    }
+    public void menuKelolaHewan(){
+        Intent intent = new Intent(MainActivity.this, KelolaHewanActivity.class);
+        startActivity(intent);
     }
 }
